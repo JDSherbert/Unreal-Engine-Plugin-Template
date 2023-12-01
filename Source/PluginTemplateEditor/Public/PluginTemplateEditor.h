6213005054be9d7fa19e2a8419206d1b94004576
JDSherbert <43964243+JDSherbert@users.noreply.github.com>
2023-12-01 11:51:40 +0000

    Create PluginTemplateEditor.h

---
+++ Source/PluginTemplateEditor/Public/PluginTemplateEditor.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "PluginTemplateEditor.generated.h"
+
+UCLASS()
+class PLUGINTEMPLATEEDITOR_API UPluginTemplateEditor : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void Init();
+
+};
